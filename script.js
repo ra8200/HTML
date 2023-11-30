92d4fb1ccee60bfcf247fdbb88fd720faf06bdbd
Razi Alawamleh <razi.alawamleh@gmail.com>
2023-11-30 02:08:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// # lets create a porfolio app
+// # lets create a new folder called portfolio
+// # lets create a new file called index.js
+// # lets create a new file called index.html
+// # lets create a new file called style.css
+// # lets create a new file called script.js
+// # lets create a new file called images.js
+
+
+console.log("This is the log for the 🔥EXTERNAL🔥JavaScript");
